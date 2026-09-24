f7a99549296b7ef30415ace82b25aa601a15a2fb
Hanna Traore <traorehanna01@gmail.com>
2026-09-24 23:31:16 +0000

    Enregistre Entreprise dans l'admin avec colonnes et recherche

--- stages/admin.py
+++ stages/admin.py
@@ -6,5 +6,6 @@ from .models import Entreprise
 
 @admin.register(Entreprise)
 class EntrepriseAdmin(admin.ModelAdmin):
+    
     list_display=["nom","ville","secteur"]
     search_fields=["nom","ville"]
